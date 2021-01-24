8e1190b92a97ea93f7ebc9156391ea5f669856a5
나상우 <robbyra@gmail.com>
2021-01-24 19:21:05 +0900

    Add react-helemt

--- src/Routes/Main/index.tsx
+++ src/Routes/Main/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Presenter from './Presenter';
+import Helmet from 'react-helmet';
 
 const Main: React.FC = () => {
-  return <Presenter />;
+  return (
+    <>
+    <Helmet>
+      <title>반창고 허브 | 어드민 페이 </title>
+    </Helmet>
+  <Presenter />
+  </>
+  )
 };
 
 export default Main;
